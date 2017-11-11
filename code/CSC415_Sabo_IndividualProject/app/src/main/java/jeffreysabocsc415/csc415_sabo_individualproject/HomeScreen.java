e23a8e5d5d4992103b19438f857f127484994874
saboj2 <saboj2@tcnj.edu>
2017-11-10 20:05:46 -0500

    final document and android studio project

---
+++ code/CSC415_Sabo_IndividualProject/app/src/main/java/jeffreysabocsc415/csc415_sabo_individualproject/HomeScreen.java
@@ -0,0 +1,22 @@
+package jeffreysabocsc415.csc415_sabo_individualproject;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class HomeScreen extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    private TextView mAboutParagraph;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+    }
+
+}
