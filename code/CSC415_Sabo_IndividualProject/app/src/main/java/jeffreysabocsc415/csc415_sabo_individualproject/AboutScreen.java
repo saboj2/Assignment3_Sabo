0acc1ca5664f0403d0e6149ea820c601db78dcad
saboj2 <saboj2@tcnj.edu>
2017-11-10 22:36:51 -0500

    added bathroom info xml

---
+++ code/CSC415_Sabo_IndividualProject/app/src/main/java/jeffreysabocsc415/csc415_sabo_individualproject/AboutScreen.java
@@ -0,0 +1,14 @@
+package jeffreysabocsc415.csc415_sabo_individualproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_page);
+
+    }
+}
